a8be7a1419e87b5efccf452b21bbda5d922fd7b7
LHL <87508425+LHL3341@users.noreply.github.com>
2023-08-30 23:57:35 +0800

    Add files via upload

---
+++ mask.py
@@ -0,0 +1,37 @@
+import random
+import torch
+def mask_text(input_ids):
+    mask_map = torch.zeros_like(input_ids)
+    mask_ids = input_ids.clone()
+    for b, input_id in enumerate(input_ids):
+        text_length = input_id.shape[0]-(input_id==0).sum()
+        
+        mask = random.randint(0,text_length-1)
+        mask_ids[b,mask] = 103
+        mask_map[b,mask] = 1
+    return mask_ids, mask_map
+
+
+def mask_image(image,avg_attention_map,num_patches=196, mask_rate=0.5):
+    patch_h = 16
+    patch_w = 16
+    b,c,h,w = image.shape
+    num_patches = (h // patch_h) * (w // patch_w)
+    # (b, c=3, h, w)->(b, n_patches, patch_size**2 * c)
+    patches = image.view(
+        b, c,
+        h // patch_h, patch_h, 
+        w // patch_w, patch_w
+    ).permute(0, 2, 4, 3, 5, 1).reshape(b, num_patches, -1)
+    batch_size = image.shape[0]
+    mask_num = int(mask_rate * num_patches)
+    mask = avg_attention_map[:,random.randint(0,avg_attention_map.shape[1]-1)].clone()
+    mask /= mask.norm(dim=-1).unsqueeze(1)
+    noise = torch.rand(batch_size,num_patches)*0.02
+
+    mask = mask+noise.cuda()
+    idx = mask.argsort(descending=True)
+    masked_idx = idx[:,:mask_num]
+    unmasked_idx = idx[:,mask_num:]
+
+    return patches,masked_idx,unmasked_idx
---
+++ utils.py
@@ -0,0 +1,384 @@
+import math
+def cosine_lr_schedule(optimizer, epoch, max_epoch, init_lr, min_lr):
+    """Decay the learning rate"""
+    lr = (init_lr - min_lr) * 0.5 * (1. + math.cos(math.pi * epoch / max_epoch)) + min_lr
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+def warmup_lr_schedule(optimizer, step, max_step, init_lr, max_lr):
+    """Warmup the learning rate"""
+    lr = min(max_lr, init_lr + (max_lr - init_lr) * step / max_step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr    
+
+def step_lr_schedule(optimizer, epoch, init_lr, min_lr, decay_rate):        
+    """Decay the learning rate"""
+    lr = max(min_lr, init_lr * (decay_rate**epoch))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr    
+        
+import numpy as np
+import io
+import os
+import time
+from collections import defaultdict, deque
+import datetime
+
+import torch
+import torch.distributed as dist
+
+class SmoothedValue(object):
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        Warning: does not synchronize the deque!
+        """
+        if not is_dist_avail_and_initialized():
+            return
+        t = torch.tensor([self.count, self.total], dtype=torch.float64, device='cuda')
+        dist.barrier()
+        dist.all_reduce(t)
+        t = t.tolist()
+        self.count = int(t[0])
+        self.total = t[1]
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median,
+            avg=self.avg,
+            global_avg=self.global_avg,
+            max=self.max,
+            value=self.value)
+
+
+class MetricLogger(object):
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+            type(self).__name__, attr))
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {}".format(name, str(meter))
+            )
+        return self.delimiter.join(loss_str)
+
+    def global_avg(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {:.4f}".format(name, meter.global_avg)
+            )
+        return self.delimiter.join(loss_str)    
+    
+    def synchronize_between_processes(self):
+        for meter in self.meters.values():
+            meter.synchronize_between_processes()
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ''
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt='{avg:.4f}')
+        data_time = SmoothedValue(fmt='{avg:.4f}')
+        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
+        log_msg = [
+            header,
+            '[{0' + space_fmt + '}/{1}]',
+            'eta: {eta}',
+            '{meters}',
+            'time: {time}',
+            'data: {data}'
+        ]
+        if torch.cuda.is_available():
+            log_msg.append('max mem: {memory:.0f}')
+        log_msg = self.delimiter.join(log_msg)
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0 or i == len(iterable) - 1:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                if torch.cuda.is_available():
+                    print(log_msg.format(
+                        i, len(iterable), eta=eta_string,
+                        meters=str(self),
+                        time=str(iter_time), data=str(data_time),
+                        memory=torch.cuda.max_memory_allocated() / MB))
+                else:
+                    print(log_msg.format(
+                        i, len(iterable), eta=eta_string,
+                        meters=str(self),
+                        time=str(iter_time), data=str(data_time)))
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('{} Total time: {} ({:.4f} s / it)'.format(
+            header, total_time_str, total_time / len(iterable)))
+        
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def compute_acc(logits, label, reduction='mean'):
+    ret = (torch.argmax(logits, dim=1) == label).float()
+    if reduction == 'none':
+        return ret.detach()
+    elif reduction == 'mean':
+        return ret.mean().item()
+
+def compute_n_params(model, return_str=True):
+    tot = 0
+    for p in model.parameters():
+        w = 1
+        for x in p.shape:
+            w *= x
+        tot += w
+    if return_str:
+        if tot >= 1e6:
+            return '{:.1f}M'.format(tot / 1e6)
+        else:
+            return '{:.1f}K'.format(tot / 1e3)
+    else:
+        return tot
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def save_on_master(*args, **kwargs):
+    if is_main_process():
+        torch.save(*args, **kwargs)
+
+
+def init_distributed_mode(args):
+    #os.environ['WORLD_SIZE'] ='1'
+    #os.environ["RANK"] ='0'
+    #os.environ["LOCAL_RANK"] ='0'
+    #os.environ['MASTER_ADDR'] = 'localhost'
+    #os.environ['MASTER_PORT'] = '11111'
+
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        args.rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+        args.gpu = int(os.environ['LOCAL_RANK'])
+    elif 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
+    else:
+        print('Not using distributed mode')
+        args.distributed = False
+        return
+
+    args.distributed = True
+
+    torch.cuda.set_device(args.gpu)
+    args.dist_backend = 'nccl'
+    print('| distributed init (rank {}, word {}): {}'.format(
+        args.rank, args.world_size, args.dist_url), flush=True)
+    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                         world_size=args.world_size, rank=args.rank)
+    torch.distributed.barrier()
+    setup_for_distributed(args.rank == 0)        
+
+def vlmatch(object_feat,text_feat):
+    threshold = 0.01
+    res = torch.einsum("oc,tc->ot",object_feat,text_feat)# [50,30]
+    pos_map = res.max(dim=1)[0] > threshold # [50]
+    if pos_map.sum() == 0:
+        return None,None
+    matched_text_feature = torch.stack([text_feat[text_features>threshold].mean(dim=0)\
+                                         for text_features in res[pos_map]])
+    matched_object_feature = object_feat[pos_map]#[n,256]
+    return matched_object_feature,matched_text_feature
+
+def calculate_giou(box_1, box_2):
+    """
+    calculate giou
+    :param box_1: (x0, y0, x1, y1)
+    :param box_2: (x0, y0, x1, y1)
+    :return: value of giou
+    """
+    # calculate area of each box
+    area_1 = (box_1[2] - box_1[0]) * (box_1[3] - box_1[1])
+    area_2 = (box_2[2] - box_2[0]) * (box_1[3] - box_1[1])
+
+    # calculate minimum external frame
+    area_c = (max(box_1[2], box_2[2]) - min(box_1[0], box_2[0])) * (max(box_1[3], box_2[3]) - min(box_1[1], box_2[1]))
+
+    # find the edge of intersect box
+    top = max(box_1[0], box_2[0])
+    left = max(box_1[1], box_2[1])
+    bottom = min(box_1[3], box_2[3])
+    right = min(box_1[2], box_2[2])
+
+    # calculate the intersect area
+    area_intersection = (right - left) * (bottom - top)
+
+    # calculate the union area
+    area_union = area_1 + area_2 - area_intersection
+
+    # calculate iou
+    iou = float(area_intersection) / area_union
+
+    # calculate giou(iou - (area_c - area_union)/area_c)
+    giou = iou - float((area_c - area_union)) / area_c
+
+    return giou
+
+def calculate_ciou(box_1, box_2):
+    """
+    calculate ciou
+    :param box_1: (x0, y0, x1, y1)
+    :param box_2: (x0, y0, x1, y1)
+    :return: value of ciou
+    """
+    # calculate area of each box
+    width_1 = box_1[2] - box_1[0]
+    height_1 = box_1[3] - box_1[1]
+    area_1 = width_1 * height_1
+
+    width_2 = box_2[2] - box_2[0]
+    height_2 = box_2[3] - box_2[1]
+    area_2 = width_2 * height_2
+
+    # calculate center point of each box
+    center_x1 = (box_1[2] - box_1[0]) / 2
+    center_y1 = (box_1[3] - box_1[1]) / 2
+    center_x2 = (box_2[2] - box_2[0]) / 2
+    center_y2 = (box_2[3] - box_2[1]) / 2
+
+    # calculate square of center point distance
+    p2 = (center_x2 - center_x1) ** 2 + (center_y2 - center_y1) ** 2
+
+    # calculate square of the diagonal length
+    width_c = max(box_1[2], box_2[2]) - min(box_1[0], box_2[0])
+    height_c = max(box_1[3], box_2[3]) - min(box_1[1], box_2[1])
+    c2 = width_c ** 2 + height_c ** 2
+
+    # find the edge of intersect box
+    left = max(box_1[0], box_2[0])
+    top = max(box_1[1], box_2[1])
+    bottom = min(box_1[3], box_2[3])
+    right = min(box_1[2], box_2[2])
+
+    # calculate the intersect area
+    area_intersection = (right - left) * (bottom - top)
+
+    # calculate the union area
+    area_union = area_1 + area_2 - area_intersection
+
+    # calculate iou
+    iou = float(area_intersection) / area_union
+
+    # calculate v
+    arctan = math.atan(float(width_2) / height_2) - math.atan(float(width_1) / height_1)
+    v = (4.0 / math.pi ** 2) * (arctan ** 2)
+
+    # calculate alpha
+    alpha = float(v) / (1 - iou + v)
+
+    # calculate ciou(iou - p2 / c2 - alpha * v)
+    ciou = iou - float(p2) / c2 - alpha * v
+
+    return ciou
