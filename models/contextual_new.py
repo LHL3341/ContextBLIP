e70ca03692fae447dbd6ebfb4cce5a6dbfe092d5
lhl <lhl3341@bupt.edu.cn>
2023-09-18 13:46:20 +0800

    1

---
+++ models/contextual_new.py
@@ -0,0 +1,147 @@
+'''
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * By Junnan Li
+'''
+from models.med import BertConfig, BertModel
+import transformers
+transformers.logging.set_verbosity_error()
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from models.blip import BLIP_Base
+
+
+class Adapter(nn.Module):
+    def __init__(self, c_in, reduction=4):
+        super(Adapter, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(c_in, c_in // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c_in // reduction, c_in, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+class DownSampler(nn.Module):
+    def __init__(self, c_in, reduction=4):
+        super(DownSampler, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(c_in, c_in // reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+    
+class UpSampler(nn.Module):
+    def __init__(self, c_in, reduction=1):
+        super(UpSampler, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(c_in // reduction, c_in, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+    
+    
+class Adapter_BLIP(nn.Module):
+    def __init__(self,                 
+                 med_config = 'configs/bert_config.json',  
+                 blip_path = 'model_base_14M.pth',
+                 image_size = 224,
+                 vit = 'base',
+                 vit_grad_ckpt = False,
+                 vit_ckpt_layer = 0,                    
+                 embed_dim = 256,     
+                 queue_size = 57600,
+                 momentum = 0.995,
+                 ):
+        """
+        Args:
+            med_config (str): path for the mixture of encoder-decoder model's configuration file
+            image_size (int): input image size
+            vit (str): model size of vision transformer
+        """               
+        super().__init__()
+        self.pretrained_blip = BLIP_Base(med_config)
+        vision_width = self.pretrained_blip.visual_encoder.embed_dim
+
+        
+        self.adapt_layer = [3,6,9,12]
+        self.vision_adapter = ([DownSampler(vision_width) for i in self.adapt_layer],UpSampler(vision_width))
+        
+    def forward(self, image, caption,output_attentions = False):
+    
+        image_embeds,hidden = self.pretrained_blip.visual_encoder(image,output_hidden_states=True) # [b,197,768]
+        image_atts = torch.ones(image_embeds.size()[:-1],dtype=torch.long).to(image.device)            
+            
+        text = self.pretrained_blip.tokenizer(caption, padding='max_length', truncation=True, max_length=30, 
+                                return_tensors="pt").to(image.device)  # [b,30]
+             
+        #image_embeds = image_embeds + self.vision_adapter(image_embeds)
+        latent_features = []
+        for i,layer in enumerate(self.adapt_layer):
+            latent = self.vision_adapter[0][i].to(image.device)(hidden[layer-1])
+            latent_features.append(latent)
+        latent_features = torch.cat(latent_features,dim=2)
+        image_embeds = image_embeds + self.vision_adapter[1].to(image.device)(latent_features)
+        ###============== Image-text Matching ===================###
+        encoder_input_ids = text.input_ids.clone()
+        encoder_input_ids[:,0] = self.pretrained_blip.tokenizer.enc_token_id
+        
+        # forward the positve image-text pair
+        bs = image.size(0)
+        output_pos = self.pretrained_blip.text_encoder(encoder_input_ids,
+                                       attention_mask = text.attention_mask,
+                                       encoder_hidden_states = image_embeds,
+                                       encoder_attention_mask = image_atts,
+                                        output_attentions = True,
+                                       return_dict = True,
+                                      ) # [b,30,768]      
+        if output_attentions:
+            attention_map = torch.stack(output_pos['cross_attentions'],dim=1)
+            avg_attention_map = attention_map.mean(dim=1).mean(dim=1).detach()
+            return output_pos.last_hidden_state, avg_attention_map    
+        return output_pos.last_hidden_state
+    
+    def inference(self,image,caption):
+        with torch.no_grad():
+        
+            image_embeds = self.pretrained_blip.visual_encoder(image) # [b,197,768]
+            image_atts = torch.ones(image_embeds.size()[:-1],dtype=torch.long).to(image.device)        
+            image_feat = F.normalize(self.pretrained_blip.vision_proj(image_embeds[:,0,:]),dim=-1) # [b,256]         
+            
+            text = self.pretrained_blip.tokenizer(caption, padding='max_length', truncation=True, max_length=30, 
+                                return_tensors="pt").to(image.device)  # [b,30]
+            text_output = self.pretrained_blip.text_encoder(text.input_ids, attention_mask = text.attention_mask,                      
+                                            return_dict = True, mode = 'text')  # [b,30,768]      
+            text_feat = F.normalize(self.pretrained_blip.text_proj(text_output.last_hidden_state[:,0,:]),dim=-1)# [b,256]                 
+             
+            image_embeds = image_embeds + self.vision_adapter(image_embeds)
+
+            ###============== Image-text Matching ===================###
+            encoder_input_ids = text.input_ids.clone()
+            encoder_input_ids[:,0] = self.pretrained_blip.tokenizer.enc_token_id
+            
+            # forward the positve image-text pair
+            bs = image.size(0)
+            output_pos = self.pretrained_blip.text_encoder(encoder_input_ids,
+                                        attention_mask = text.attention_mask,
+                                        encoder_hidden_states = image_embeds,
+                                        encoder_attention_mask = image_atts,
+                                            output_attentions = True,
+                                        return_dict = True,
+                                        ) # [b,30,768] 
+            logits = self.pretrained_blip.itm_head(output_pos)
+            return logits
---
+++ zero-shot_new.py
@@ -0,0 +1,145 @@
+# inspired from: https://github.com/openai/CLIP/issues/83
+# https://github.com/openai/CLIP/issues/83
+import warnings
+warnings.filterwarnings("ignore")
+import json
+import os
+import random
+import torch
+from torch import autograd
+import tqdm
+from torch import nn, optim
+import torch.nn.functional as F
+from PIL import Image
+from pathlib import Path
+from collections import defaultdict
+import sys
+import torchvision.transforms as transforms
+from volta_src.config import BertConfig
+from models.vit import VisionTransformer, interpolate_pos_embed
+from models.med import BertConfig, BertModel, BertLMHeadModel 
+from volta_src.embeddings import BertLayerNorm
+from volta_src.encoders import GeLU
+from extras_ import convert_sents_to_features, BertLayer
+import argparse
+from urllib.parse import urlparse
+from timm.models.hub import download_cached_file
+
+from transform.randaugment import RandomAugment
+from torchvision.transforms.functional import InterpolationMode
+
+from models.contextual_new import Adapter_BLIP
+import yaml
+
+random.seed(10)
+torch.manual_seed(10)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--finetuned_checkpoint_path", default='output/Pretrain/checkpoint_00.pth')
+
+parser.add_argument('--valid_descr_path', type=str, default='./data/valid_data.json')
+parser.add_argument('--train_descr_path', type=str, default='./data/train_data.json')
+parser.add_argument('--imgs_path', type=str, default='./data/image-sets')
+
+args = parser.parse_args()
+with open('analysis/manual_annotation_valid.yaml')as f:
+    annotations = yaml.load(f.read(), Loader=yaml.FullLoader)
+phenomenons = defaultdict(int)
+correct_cases = defaultdict(int)
+acc_each_case = defaultdict(float)
+whole_name = {'s':'Spatial Relations(空间关系)',
+              'col':'Colors(颜色)',
+              'q':'Quantities(量词)',
+              'n':'Nuances(细微、偏僻部分)',
+              'con':'Context(上下文)',
+              'img':'Unknown Type',
+              'neg':'Negation(否定)',
+              'v':'Visibility(不可见、遮挡)',
+              'cor':'Co-reference(指代)',
+              't':'Temporal(时序)',
+                '': 'Others'
+              }
+for key,value in annotations.items():
+    for k, v in value.items():
+        annotation = v['annotation'].split(',')
+        for case in annotation:
+            phenomenons[case] +=1
+print('phenomenons:',phenomenons)
+
+    
+img_dirs = args.imgs_path
+valid_data = json.load(open(args.valid_descr_path, 'r'))
+train_data = json.load(open(args.train_descr_path, 'r'))
+train = []
+for img_dir, data in train_data.items():
+    for img_idx, text in data.items():
+        train.append((img_dir, img_idx, text))
+valid = []
+for img_dir, data in valid_data.items():
+    for img_idx, text in data.items():
+        valid.append((img_dir, img_idx, text))
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f'DEVICE USED: {device}')
+
+
+bert_config = json.load(open('vilbert-and-bert-config.json', 'r'))
+model = Adapter_BLIP()
+checkpoint = torch.load(args.finetuned_checkpoint_path)
+model.load_state_dict(checkpoint['model'],strict= False)
+
+
+correct = 0
+total = 0
+video_correct = 0
+video_total = 0
+img_correct = 0
+img_total = 0
+ranks = defaultdict(int)
+model.cuda()
+model.eval()
+for img_dir, img_idx, text in tqdm.tqdm(valid):
+    text = [text]
+    img_idx = int(img_idx)
+    img_files = list((Path(img_dirs) / img_dir).glob("*.jpg"))
+    img_files = sorted(img_files, key=lambda x: int(str(x).split('/')[-1].split('.')[0][3:]))
+    images = [Image.open(photo_file).convert("RGB") for photo_file in img_files]
+    preprocess = transforms.Compose([
+        transforms.Resize((224,224),interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        ])  
+    images = [preprocess(image) for image in images]
+    image = torch.stack(images).to(device)
+    with torch.no_grad():
+        output = model(image, text).squeeze()
+        logits = model.pretrained_blip.itm_head(output[:,0,:])
+        itm_score = torch.nn.functional.softmax(logits,dim=1)[:,1]
+    pred = torch.argmax(itm_score).squeeze()
+    if img_idx == pred:
+        correct += 1
+        if img_dir in annotations.keys():
+            if str(img_idx) in annotations[img_dir].keys():
+                phenomenon = annotations[img_dir][str(img_idx)]['annotation'].split(',')
+                for case in phenomenon:
+                    correct_cases[case]+=1
+    if 'open-images' in img_dir:
+        img_total += 1
+        if img_idx == pred:
+            img_correct += 1
+    else:
+        video_total += 1
+        if img_idx == pred:
+            video_correct += 1        
+    total += 1
+print(len(valid))
+print(ranks)
+acc = round(correct / total, 4)
+video_acc = round(video_correct / video_total, 4)
+img_acc = round(img_correct / img_total, 4)
+print("TOTAL ACC:",acc)
+print("STATIC ACC:",img_acc)
+print("VIDEO ACC:",video_acc)
+for case in correct_cases.keys():
+    acc_each_case[whole_name[case]] = round(correct_cases[case]/phenomenons[case],4)
+print("acc in different cases:",acc_each_case)
+
