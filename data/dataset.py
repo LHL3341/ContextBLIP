874863d0996ff242b3a68dcfbaf33d2f7c3b483c
lhl <lhl3341@bupt.edu.cn>
2023-09-26 23:50:29 +0800

    1

--- data/dataset.py
+++ data/dataset.py
@@ -4,7 +4,10 @@ from functools import partial
 
 import torch
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
 from torchvision.transforms import Pad, Resize, ToTensor, Compose
+from torchvision.transforms.functional import InterpolationMode
+from transform.randaugment import RandomAugment
 # from transformers import BertTokenizerFast
 from PIL import Image
 import json
@@ -30,10 +33,22 @@ def default_text_transform(text, tokenizer, max_length=77):
     )
     return inputs
 
-
+transform_train = transforms.Compose([                        
+        transforms.RandomResizedCrop(224,scale=(0.2, 1.0),interpolation=InterpolationMode.BICUBIC),
+        transforms.RandomHorizontalFlip(),
+        RandomAugment(2,5,isPIL=True,augs=['Identity','AutoContrast','Brightness','Sharpness','Equalize',
+                                        'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+    ]) 
+transform_test = transforms.Compose([
+            transforms.Resize((224,224),interpolation=InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+            ])  
 class ImageCoDeDataset(Dataset):
 
-    def __init__(self, data_dir, split, image_transform=None, text_transform=None, video_only=False):
+    def __init__(self, data_dir, split, split_text=False,image_transform=None, text_transform=None, video_only=False):
         super().__init__()
         assert split in ['train', 'valid']
     
@@ -41,6 +56,10 @@ class ImageCoDeDataset(Dataset):
             self.image_transform = image_transform
         else:
             self.image_transform = default_image_transform
+        if split == 'train':
+            self.image_transform = transform_train
+        if split == 'valid':
+            self.image_transform = transform_test
         
         # if text_transform is not None:
         self.text_transform = text_transform
@@ -48,10 +67,14 @@ class ImageCoDeDataset(Dataset):
         #     self.tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
         #     self.text_transform = partial(default_text_transform, tokenizer=self.tokenizer)
 
-        self.data = self.load_data(Path(data_dir), split, video_only)
+        self.data = self.load_data(Path(data_dir),Path(data_dir,'image-sets'), split, video_only,split_text=split_text)
 
     @staticmethod
-    def load_data(data_dir, img_path, split, video_only=False):
+    def load_data(data_dir, img_path, split, video_only=False,split_text=False):
+        if split_text:
+            with open(data_dir / f'{split}_split.json') as f:
+                split_data = json.load(f)
+
         with open(data_dir / f'{split}_data.json') as f:
             json_file = json.load(f)
 
@@ -64,15 +87,18 @@ class ImageCoDeDataset(Dataset):
                 if video_only:
                     if not static:
                         dataset.append((img_dir, img_files, int(img_idx), text))
+                        if split_text and text in split_data:
+                            dataset += [(img_dir, img_files,int(img_idx), sentence) for sentence in split_data[text]]
                 else:
                     dataset.append((img_dir, img_files, int(img_idx), text))
-        
+                    if split_text and text in split_data:
+                        dataset += [(img_dir, img_files,int(img_idx), sentence) for sentence in split_data[text]]
         return dataset
     
     def __getitem__(self, idx):
         img_dir, img_files, img_idx, text = self.data[idx]
         
-        images = [self.image_transform(Image.open(img_file)) for img_file in img_files]
+        images = [self.image_transform(Image.open(img_file).convert('RGB')) for img_file in img_files]
         img = torch.stack(images, dim=0)
         
         txt = text#self.text_transform(text)
